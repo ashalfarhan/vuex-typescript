03c090ec239ffc1d7ebe9b2211213a4441de6065
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-20 19:59:36 +0700

    create form components

---
+++ src/components/Post.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Add your post here</h1>
+    <div>
+      <form>
+        <div>
+          <label>Title</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label>Body:</label>
+          <input type="text" />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Form",
+});
+</script>
