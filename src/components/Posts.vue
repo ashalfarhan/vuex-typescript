e4b821066d75d228f07dbb342835fb3d38447416
ashalfarhan8 <ashalfarhan8@gmail.com>
2021-02-20 19:59:26 +0700

    mapping posts

---
+++ src/components/Posts.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Post App</h1>
+    <div>
+      <div v-for="post in posts" :key="post.id">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.body }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, ref } from "vue";
+import { useStore } from "@/store";
+import { TAction } from "@/types";
+
+export default defineComponent({
+  name: "Posts",
+  setup() {
+    const store = useStore();
+    const posts = computed(() => store.getters.getAll);
+    return {
+      posts,
+    };
+  },
+});
+</script>
+
+<style></style>
